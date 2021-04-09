1c9fce0de332919d6641cab67c528fb8a314b852
Aslan Aimakov <sstylee228@gmail.com>
2021-04-09 15:07:04 +0800

    done with sift+flann, testing version ready

--- ass1.py
+++ ass1.py
@@ -24,77 +24,102 @@ def readfile_as_dict(path):
                 ans_dict[key] = value
     return ans_dict
 
-def calculateDistance(im1, im2):
-    return np.sum((im1-im2)**2)
 
-def calcEuclidean(im1, im2):
-    df = np.asarray(im1 - im2)
-    return np.sqrt(np.sum(df**2, axis=1))
+def sift_flann(im_query, im, i):
+    sift = cv.SIFT_create()
+    kp1, des1 = sift.detectAndCompute(im_query, None)
+    kp2, des2 = kp_desc_dict[i]
 
-ground_truth_dict = readfile_as_dict('examples\\example_result\\rank_groundtruth.txt')
-ranklist_example_dict = readfile_as_dict('examples\\example_result\\rankList.txt')
+    index_params = dict(algorithm = 1, trees = 5)
+    search_params = dict(checks = 50)
 
-# NOTE THAT INDEX STARTS FROM ZERO WHILE IMAGE NUMBERS START FROM 1
-images_range = 1001
+    flann = cv.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des1, des2, k = 2)
+    good_matches = [m for m, n in matches if m.distance < 0.89 * n.distance]
 
-images = [cv.imread('Images\\{}.jpg'.format(str(file).zfill(4)), 0) for file in range(1, images_range)]
-given_boundary_boxes = [open('Images\\{}'.format(file)).readline().rstrip('\n').split() for file in os.listdir('Images') if file.endswith('.txt')]
-given_boundary_boxes = [[int(x) for x in row] for row in given_boundary_boxes[:images_range]]
-images_cropped = [image[box[1]:box[1]+box[3], box[0]:box[0]+box[2]] for image, box in zip(images, given_boundary_boxes)]
+    print('There are %d good matches' % len(good_matches))
+    similarity = (len(good_matches) * 100) / len(kp2)
+    print('Similarity is %.2f percent' % similarity)
 
-query_images = [cv.imread('Queries\\{}'.format(file), 0) for file in os.listdir('Queries') if file.endswith('.jpg')]
-query_boundary_boxes = [open('Queries\\{}'.format(file)).readline().rstrip('\n').split() for file in os.listdir('Queries') if file.endswith('.txt')]
-query_boundary_boxes = [[int(x) for x in row] for row in query_boundary_boxes]
-query_images_cropped = [image[box[1]:box[1]+box[3], box[0]:box[0]+box[2]] for image, box in zip(query_images, query_boundary_boxes)]
+    if len(good_matches) > 5:
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 
-im_query = query_images_cropped[0]
-im = images_cropped[252]
+        M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
+        matchesMask = mask.ravel().tolist()
+        h, w = im_query.shape
+        pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
+        dst = cv.perspectiveTransform(pts, M)
 
-orb = cv.ORB_create()
-sift = cv.SIFT_create()
-bf = cv.BFMatcher()
+        im = cv.polylines(im, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
 
-kp1, des1 = sift.detectAndCompute(im_query, None)
-kp2, des2 = sift.detectAndCompute(im, None)
+    else:
+        print ("Not enough matches are found - {}/{}".format(len(good), 10))
+        matchesMask = None
 
-index_params = dict(algorithm = 0, trees = 5)
-search_params = dict(checks = 50)
+    if matchesMask is None:
+        print('trash')
+    else:
+        inlier_matches = sum(matchesMask)
 
-# flann = cv.FlannBasedMatcher(index_params, search_params)
-# matches = flann.knnMatch(des1, des2, k = 2)
+    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                       singlePointColor = None,
+                       matchesMask = matchesMask, # draw only inliers
+                       flags = cv.DrawMatchesFlags_DEFAULT)
 
-matches = bf.knnMatch(des1, des2, k = 2)
-good = [[m] for m, n in matches if m.distance < 0.75 * n.distance]
-matches_mask = [[1, 0] if m.distance < 0.75 * n.distance else [0, 0] for (m, n) in matches]
-print('There are %d good matches' % len(good))
-print('Similarity is %.2f percent' % ((len(good) * 100) / len(kp2)))
-print(len(good), len(kp1), len(kp2))
+    im_final = cv.drawMatches(im_query, kp1, im, kp2, good_matches, None, **draw_params)
 
-# if len(good) > 10:
-#     src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-#     dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
-#
-#     M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
-#     matchesMask = mask.ravel().tolist()
-#
-#     h, w = im_query.shape
-#     pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
-#     dst = cv.perspectiveTransform(pts, M)
-#
-#     im = cv.polylines(im, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
-#
-# else:
-#     print ("Not enough matches are found - {}/{}".format(len(good), 10))
-#     matchesMask = None
-#
-# draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-#                    singlePointColor = None,
-#                    matchesMask = matches_mask, # draw only inliers
-#                    flags = 2)
-#
-# im_final = cv.drawMatches(im_query, kp1, im, kp2, good, None, **draw_params)
+    return inlier_matches
 
-im_final = cv.drawMatchesKnn(im_query, kp1, im, kp2, good, None, flags = 2)
+if __name__ == '__main__':
+    # NOTE THAT INDEX STARTS FROM ZERO WHILE IMAGE NUMBERS START FROM 1
 
-plt.imshow(im_final)
-plt.show()
+    images = [cv.imread('Images\\{}.jpg'.format(str(file).zfill(4)), 0) for file in range(1, 5001)]
+    given_boundary_boxes = [open('Images\\{}'.format(file)).readline().rstrip('\n').split() for file in os.listdir('Images') if file.endswith('.txt')]
+    given_boundary_boxes = [[int(x) for x in row] for row in given_boundary_boxes]
+    images_cropped = [image[box[1]:box[1]+box[3], box[0]:box[0]+box[2]] for image, box in zip(images, given_boundary_boxes)] + [image for image in images[2000:]]
+
+    query_images = [cv.imread('Queries\\{}'.format(file), 0) for file in os.listdir('Queries') if file.endswith('.jpg')]
+    query_boundary_boxes = [open('Queries\\{}'.format(file)).readline().rstrip('\n').split() for file in os.listdir('Queries') if file.endswith('.txt')]
+    query_boundary_boxes = [[int(x) for x in row] for row in query_boundary_boxes]
+    query_images_cropped = [image[box[1]:box[1]+box[3], box[0]:box[0]+box[2]] for image, box in zip(query_images, query_boundary_boxes)]
+
+    example_query_images = [cv.imread('examples\\example_query\\{}'.format(file), 0) for file in os.listdir('examples\\example_query') if file.endswith('.jpg')]
+    example_query_boundary_boxes = [open('examples\\example_query\\{}'.format(file)).readline().rstrip('\n').split() for file in os.listdir('examples\\example_query') if file.endswith('.txt')]
+    example_query_boundary_boxes = [[int(x) for x in row] for row in example_query_boundary_boxes]
+    example_query_images_cropped = [image[box[1]:box[1]+box[3], box[0]:box[0]+box[2]] for image, box in zip(example_query_images, example_query_boundary_boxes)]
+
+    sift = cv.SIFT_create()
+    kp_desc_dict = []
+    for i, image in enumerate(images_cropped):
+        print('Calculating descriptors for image #%d' % (i + 1))
+        kp_desc_dict.append(sift.detectAndCompute(image, None))
+    # kp_desc_dict = [sift.detectAndCompute(image, None) for image in images_cropped]
+
+    # GET RANKLIST FOR 20 QUERY IMAGES
+    # rank_list = open('rankList.txt', mode = 'a')
+    # for i, query_image in enumerate(query_images_cropped):
+    #     print('Query #%d starts' % (i + 1))
+    #
+    #     single_rank_list = {str(j + 1): sift_flann(query_image, image, j) for j, image in enumerate(images_cropped)}
+    #     print ('single_rank_list # %d is done' % (i + 1))
+    #
+    #     line = 'Q{}: '.format(i + 1) + ' '.join(str(key) for key in sorted(single_rank_list.items(), key = lambda x: x[1], reverse = True).keys())
+    #     print('Query #%d is done' % (i + 1))
+    #     rank_list.write(line)
+    #
+    # rank_list.close()
+
+    # GET RANKLIST FOR 10 EXAMPLE IMAGES (FOR PERFORMANCE EVALUATION)
+    rank_list = open('rankList.txt', mode = 'a')
+    for i, example_image in enumerate(example_query_images_cropped):
+        print('Example image #%d starts' % (i + 1))
+
+        single_rank_list = {str(j + 1): sift_flann(example_image, image, j) for j, image in enumerate(images_cropped)}
+        print ('single_rank_list # %d is done' % (i + 1))
+
+        line = 'Q{}: '.format(i + 1) + ' '.join(str(key) for key in sorted(single_rank_list.items(), key = lambda x: x[1], reverse = True))
+        print('Example image #%d is done' % (i + 1))
+        rank_list.write(line)
+
+    rank_list.close()
